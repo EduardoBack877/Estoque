58155a4ddfb275aa2b87140fa727d1d5b9b048e4
Eduardo Back <Eduardoback87@gmail.com>
2021-06-19 17:31:01 -0300

    removido uma linha de import para resolver problema do jenkins (ignorar))

---
+++ test/ProdutoDAOTest.java
@@ -0,0 +1,44 @@
+
+import apoio.ConexaoBD;
+import dao.ProdutoDAO;
+import entidade.Produto;
+import java.sql.Connection;
+import java.sql.SQLException;
+import static junit.framework.Assert.assertTrue;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+
+public class ProdutoDAOTest {
+   private static ProdutoDAO produtoDAO;
+   
+   @BeforeClass
+   public static void init () throws SQLException
+   {
+    Connection conexao = ConexaoBD.getInstance().getConnection();
+     produtoDAO = new ProdutoDAO();
+     
+     
+   }
+   
+@Test
+   public void testeSalvar() throws Exception {
+     int aux = 0;
+     Produto prod = new Produto(); 
+     prod.setCodgrupo(1);
+     prod.setCodprat(1);
+     prod.setCodsecao(1);
+     prod.setCor("Amarelo");
+     prod.setDescricao("Blazer");
+     prod.setMarca("Nike");
+     prod.setQtd(10);
+     prod.setTamanho("M");
+     if (produtoDAO.salvar(prod)) {
+         aux = 1;
+     };
+     assertTrue(aux == 1);
+ 
+    }
+}
