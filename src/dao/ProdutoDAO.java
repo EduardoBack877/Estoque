8006de120abd52ec0aa0e672fd1878e83762bce3
Eduardo Back <Eduardoback87@gmail.com>
2021-06-19 14:33:11 -0300

    removido uma linha de import para resolver problema do jenkins (ignorar))

--- src/dao/ProdutoDAO.java
+++ src/dao/ProdutoDAO.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.scene.control.ComboBox;
 import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
